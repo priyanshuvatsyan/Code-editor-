3d01406e2c0473063fe3d60c944665ee368a9699
Priyanshu <56836660+priyanshuvatsyan@users.noreply.github.com>
2024-04-10 22:35:28 +0530

    Theme, Font size features added

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function run() {
 
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Your code here
     function saveCode() {
         console.log("save data triggered");
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         console.log("data saved");
     }
-    
+
 
     function retrieveCode() {
         let html = localStorage.getItem("html");
@@ -43,3 +43,44 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("load", retrieveCode);
 });
 
+
+function FontSize(size) {
+   /*  console.log("trigger 1"); */
+    var elements = document.querySelectorAll('#html, #css, #js');
+   /*  console.log("trigger 2"); */
+    elements.forEach(function (element) {
+        switch (size) {
+            case 'small':
+                /* console.log("triggered small "); */
+                element.style.fontSize = '15px';
+                break;
+
+            case 'medium':
+               /*  console.log("triggered medium "); */
+                element.style.fontSize = '20px';
+                break;
+
+            case 'large':
+               /*  console.log("triggered large "); */
+                element.style.fontSize = '25px';
+                break;
+        }
+    })
+}
+
+function theme() {
+    /* console.log("trigger 1"); */
+    var elements = document.querySelectorAll('textarea');
+    var ogColor = '';
+    if (elements.length > 0) {
+        ogColor = elements[0].style.backgroundColor || getComputedStyle(elements[0]).backgroundColor;
+    }
+   /*  console.log("trigger 2"); */
+    var newColor = (ogColor === 'rgb(255, 255, 255)') ? '' : 'rgb(255, 255, 255)';
+    elements.forEach(function (el) {
+        el.style.backgroundColor = newColor;
+    })
+var themeIcon = document.getElementById('themeIcon');
+themeIcon.style.color = "#454545";
+   /*  console.log(" final trigger "); */
+}
